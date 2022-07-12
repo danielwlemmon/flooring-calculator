710237a946cb6e9b6dbd95d58470ea25c163b3a5
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-07-12 15:38:51 -0600

    cleared boiler plate formatting

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,42 +1,45 @@
 import React from "react";
 
-function Footer () {
-  return(
-    <div>
+function Footer() {
+  return (
+    <div className="footer">
       <table>
-        <tr>
-          <td>1/16</td>
-          <td>1/8</td>
-          <td>3/16</td>
-          <td>1/4</td>
-          <td>5/16</td>
-          <td>3/8</td>
-          <td>7/16</td>
-          <td>1/2</td>
-          <td>9/16</td>
-          <td>5/8</td>
-          <td>11/16</td>
-          <td>3/4</td>
-          <td>13/16</td>
-          <td>7/8</td>
-          <td>15/16</td>
-        </tr>
-          <td>0.0625</td>
-          <td>0.125</td>
-          <td>0.1875</td>
-          <td>0.25</td>
-          <td>0.3125</td>
-          <td>0.375</td>
-          <td>0.4375</td>
-          <td>0.5</td>
-          <td>0.5625</td>
-          <td>0.625</td>
-          <td>0.6875</td>
-          <td>0.75</td>
-          <td>0.8125</td>
-          <td>0.875</td>
-          <td>0.9375</td>
-        <tr/>
+        <tbody>
+          <tr>
+            <td>1/16</td>
+            <td>1/8</td>
+            <td>3/16</td>
+            <td>1/4</td>
+            <td>5/16</td>
+            <td>3/8</td>
+            <td>7/16</td>
+            <td>1/2</td>
+            <td>9/16</td>
+            <td>5/8</td>
+            <td>11/16</td>
+            <td>3/4</td>
+            <td>13/16</td>
+            <td>7/8</td>
+            <td>15/16</td>
+          </tr>
+          <tr>
+            <td>0.0625</td>
+            <td>0.125</td>
+            <td>0.1875</td>
+            <td>0.25</td>
+            <td>0.3125</td>
+            <td>0.375</td>
+            <td>0.4375</td>
+            <td>0.5</td>
+            <td>0.5625</td>
+            <td>0.625</td>
+            <td>0.6875</td>
+            <td>0.75</td>
+            <td>0.8125</td>
+            <td>0.875</td>
+            <td>0.9375</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
